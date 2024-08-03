3afec914967948d66d258597a9d062b6ccf7550c
abhijith553 <abhijith1375@yahoo.com>
2024-08-03 12:31:16 +0530

    made changes to voltage data

--- lab-3-qns1.r
+++ lab-3-qns1.r
@@ -1,4 +1,4 @@
-voltageData = c(235, 223, 224, 220, 230, 228, 241, 256, 239, 238);
+voltageData = c(235, 223, 224, 220, 230, 228, 241, 256, 239, 238, 300);
 mean = mean(voltageData);
 variance = var(voltageData);
 cat(mean,'\n')
